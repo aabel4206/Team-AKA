8b7583875d4eb422e65e58bad7b7430034d6a77a
Kidus Beshah <kidus1999amare@gmail.com>
2026-03-22 19:20:28 -0500

    Add phase 1 board, position, and player classes

--- src/chess/game/Player.java
+++ src/chess/game/Player.java
@@ -1,7 +1,29 @@
 package chess.game;
 
-/**
- * Represents a player.
- */
 public class Player {
+
+    private final String name;
+    private final String color;
+
+    public Player(String color) {
+        this(color, color);
+    }
+
+    public Player(String name, String color) {
+        this.name = name;
+        this.color = color;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
 }
